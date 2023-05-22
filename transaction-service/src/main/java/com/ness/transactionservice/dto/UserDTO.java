20a2b4e9a9eb5b633345a07580aa901f05f89f2c
Nitish <nitish778191@gmail.com>
2023-05-22 09:30:11 +0530

    committing by month,day,year transacation and mockito test cases

---
+++ transaction-service/src/main/java/com/ness/transactionservice/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.ness.transactionservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private int userId;
+    private String userName;
+    private int userLimit;
+}
